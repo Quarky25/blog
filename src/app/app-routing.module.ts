98748d206b8d2519d824281cce6f013bdc5481f2
Quarky25 <ralphganser4@gmail.com>
2024-11-14 20:47:31 +0100

    fixed styling issues and added burger menu on sm screens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { PostDetailsComponent } from './post-details/post-details.component';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [
   {path:"", component: HomePageComponent},
@@ -14,7 +19,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), MatMenuTrigger, MatMenu, MatMenuItem, MatListModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  exports: [RouterModule, MatMenuTrigger, MatMenu, MatMenuItem, MatListModule, MatToolbarModule, MatButtonModule, MatIconModule]
 })
 export class AppRoutingModule { }
